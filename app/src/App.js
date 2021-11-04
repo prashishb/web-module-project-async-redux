ae0dc18d5f431b5aa8e21d524aed01cc9d66e4ce
prashishb <prashb.web@gmail.com>
2021-11-04 01:18:01 -0400

    footer section added

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Anime from './components/Anime';
+import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar />
       <Anime />
+      <Footer />
     </>
   );
 }
